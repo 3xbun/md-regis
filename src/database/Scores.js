bda7a98af5f843363bd9f27baa8dc080b5d867dc
Bunnasorn Kaewsiri <bunnasorn.k@outlook.com>
2023-06-13 10:03:21 +0700

    Create dummy data

---
+++ src/database/Scores.js
@@ -0,0 +1,30 @@
+const Scores = [
+    {
+        username: "bunnasorn.k",
+        works: [
+            {
+                workID: "1",
+                score: 5,
+            },
+            {
+                workID: "3",
+                score: 18,
+            }
+        ]
+    },
+    {
+        username: "dummy.j",
+        works: [
+            {
+                workID: "1",
+                score: 5,
+            },
+            {
+                workID: "3",
+                score: 18,
+            }
+        ]
+    }
+]
+
+export default Scores
---
+++ src/database/Works.js
@@ -0,0 +1,22 @@
+const Works = [
+    {
+        workID: "1",
+        title: "Work #1",
+        score: 10,
+        notion: "https://2xbun.notion.site/1-Be-A-Prompter-b04f48a67fb54f8db29dbcefb43f0192"
+    },
+    {
+        workID: "2",
+        title: "Work #2",
+        score: 50,
+        notion: "https://2xbun.notion.site/1-Be-A-Prompter-b04f48a67fb54f8db29dbcefb43f0192"
+    },
+    {
+        workID: "3",
+        title: "Work #3",
+        score: 20,
+        notion: "https://2xbun.notion.site/1-Be-A-Prompter-b04f48a67fb54f8db29dbcefb43f0192"
+    }
+]
+
+export default Works
